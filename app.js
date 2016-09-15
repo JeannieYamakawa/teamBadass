f31be053319e3398c121c3c1c68173e079fd57db
Tosin <midastouchproductions@gmail.com>
2016-09-15 18:46:19 -0500

    add hope function

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 function sillyMe(){
     console.log("Tosin is amazeballs for helpin' me learn!!");
 }
+
+function hope(){
+  console.log("I believe in a thing called love");
+}
+
+sillyMe();
+hope();
